6e2c3cc6845a2c4f147126dd1cf963af63bce22a
SevanBadal <sgolnaza@gmail.com>
2023-12-18 22:33:06 -0500

    calculate factorial recursive and while loop

--- src/calculateFactorial/index.js
+++ src/calculateFactorial/index.js
@@ -1,6 +1,23 @@
-const calculateFactorial = (n) => {
+// direct recursion
+const rCalculateFactorial = (n) => {
     if (n <= 1) return 1
-    return n * calculateFactorial(n - 1)
+    return n * rCalculateFactorial(n - 1)
+}
+
+const calculateFactorialLoop = (n) => {
+    let result = 1
+    while (n > 1) {
+        result *= n
+        n--
+    }
+    return result
+}
+
+
+// Tail Recursion ** Node doesn't optimize for this so linear space complexity (O(n))
+const calculateFactorial = (n, acc = 1) => {
+    if (n <= 1) return acc
+    return calculateFactorial(n - 1, acc * n)
 }
 
 module.exports = calculateFactorial
