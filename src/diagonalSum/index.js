0258fbc39fbf527cf7ecbb4b6d77130211671da2
SevanBadal <sgolnaza@gmail.com>
2023-12-29 16:12:28 -0500

    diagonalSum edit comments

--- src/diagonalSum/index.js
+++ src/diagonalSum/index.js
@@ -1,6 +1,6 @@
 function diagonalSum(matrix) {
     const columnBounds = matrix[0]?.length ? matrix[0].length - 1 : 0
-    const rowBounds = matrix.length - 1// assuming symmetrical matrix
+    const rowBounds = matrix.length - 1// assuming square or rectangular matrix
     let i = 0, j = 0, sum = 0
     while (i <= rowBounds && j <= columnBounds) {
         sum += matrix[i][j]
